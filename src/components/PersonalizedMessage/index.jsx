bf15f34f8ba58e2f4ca0faaecb73a885a2607942
IgorTraspadini <igorchieppe@hotmail.com>
2024-03-04 08:34:20 -0300

    On PersonalizedMessage component, included the message content the person name, place, season and interest.

--- src/components/PersonalizedMessage/index.jsx
+++ src/components/PersonalizedMessage/index.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
-
-function PersonalizedMessage() {
+function PersonalizedMessage(props) {
   return (
     <>
       <div>
-        <p>Hi</p>
+        <p>Hi {props.name}, you're going to {props.place} in {props.season}!</p>
+        <p>Here's an intinerary of things you should see, based on </p>
+        <p>{props.interest}</p>
       </div>
     </>
   )
 }
 
 export default PersonalizedMessage;
+
