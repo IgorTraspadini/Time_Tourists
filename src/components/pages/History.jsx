921d17c9d073e96c2fc7a39e9c2bfaa9a35862f8
Rachel Atkinson <r.d.atkinson97@gmail.com>
2024-03-05 14:28:07 +0000

    new history styling

--- src/components/pages/History.jsx
+++ src/components/pages/History.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import FactsandFigures from "../FactsAndFigures";
+import FactsAndFigures from "../FactsAndFigures";
 import PersonalizedMessage from "../PersonalizedMessage";
 import Wrapper from "../Wrapper";
 import { useUserContext } from "../../context/UserContext";
@@ -8,19 +8,19 @@ function History() {
   const { user, selection } = useUserContext();
 
   return (
-    <Wrapper className="bg-light-blue p-4 min-h-screen">
+    <Wrapper className="bg-creamy-yellow p-4 min-h-screen">
       <div className="max-w-4xl mx-auto">
-      <PersonalizedMessage
-        name={user.name}
-        place={selection.where}
-        season={selection.when}
-        interest={selection.interest}
-      />
-      <FactsandFigures
-        place={selection.where}
-        season={selection.when}
-        interest={selection.interest}
-      />
+        <PersonalizedMessage
+          name={user.name}
+          place={selection.where}
+          season={selection.when}
+          interest={selection.interest}
+        />
+        <FactsAndFigures
+          place={selection.where}
+          season={selection.when}
+          interest={selection.interest}
+        />
       </div>
     </Wrapper>
   );
