d23a6aa6744d5c69700410769a97236b6ead98df
Rachel Atkinson <r.d.atkinson97@gmail.com>
2024-03-05 13:15:21 +0000

    replaced color scheme config in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,11 @@ export default {
     extend: {
       colors: {
         'white': '#FFFFFF',
-        'dark-yellow': '#E5A13E',
-        'light-blue': '#CFE7E5',
-        'dark-green': '#213638',
-        'bright-blue': '#4CC9F0'
+        'mint-green': '#D3F8E2', // replacing the existing colors
+        'lavender': '#E4C1F9', // replacing the existing colors
+        'soft-pink': '#F694C1', // replacing the existing colors
+        'creamy-yellow': '#EDE7B1', // replacing the existing colors
+        'sky-blue': '#A9DEF9' // replacing the existing colors
       }
     },
   },
