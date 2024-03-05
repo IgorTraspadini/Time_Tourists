34f944f559ac2342709c35fd336b9472aa13aead
Rachel Atkinson <r.d.atkinson97@gmail.com>
2024-03-05 11:28:02 +0000

    added factsandfigures styling

--- src/components/FactsAndFigures/index.jsx
+++ src/components/FactsAndFigures/index.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import ChatComponent from "../Chatgpt_API";
 
 function FactsandFigures(props) {
-  return (<ChatComponent where={props.place} when={props.season} interest={props.interest}/>)  
+  <div className="facts-and-figures-container my-8 mx-auto p-4 bg-light-blue rounded-lg shadow">
+  <h2 className="text-dark-green text-xl font-bold mb-4">Your top picks</h2>
+  return (<ChatComponent where={props.place} when={props.season} interest={props.interest}/>)
+  </div>
 }
 
 export default FactsandFigures;
