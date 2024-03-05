cdf7b4b2512b0670e357f429235ab0b2d499c58f
Rachel Atkinson <r.d.atkinson97@gmail.com>
2024-03-05 14:36:47 +0000

    new personalized message styling

--- src/components/PersonalizedMessage/index.jsx
+++ src/components/PersonalizedMessage/index.jsx
@@ -3,13 +3,14 @@ import React from "react";
 function PersonalizedMessage(props) {
   return (
     <>
-      <div className="py-5 my-5 bg-light-blue rounded-lg p-4 shadow">
-        <p className="text-dark-green font-semibold">Hi {props.name}, you're going to {props.place} in {props.season}!</p>
-        <p className="text-dark-yellow mt-2">Here's an intinerary of things you should see, based on {props.interest}</p>
+      <div className="py-5 my-5 bg-creamy-yellow rounded-lg p-4 shadow">
+        <p className="text-muted-purple font-semibold">Hi {props.name}, you're going to {props.place} in {props.season}!</p>
+        <p className="text-muted-purple mt-2">Here's an itinerary of things you should see, based on {props.interest}</p>
       </div>
     </>
   )
 }
 
 export default PersonalizedMessage;
+
 
