3850b998cebef16fe19995d26b91438e12444647
FolaBookie <folabookie@gmail.com>
2024-03-07 12:42:39 +0000

    fixed route issues for contact and services

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,38 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import React from "react";
 import Navbar from "./components/Navbar";
-import AnimatedGlobe from '../src/components/AnimatedGlobe/index.jsx';
+import AnimatedGlobe from "../src/components/AnimatedGlobe/index.jsx";
 import SignIn from "./components/SignIn";
 import Footer from "./components/Footer";
 import History from "./components/pages/History";
-import About from "./components/pages/about.jsx"
+import About from "./components/pages/about.jsx";
 import UserSelectForm from "./components/pages/UserSelectForm";
 import UserContextProvider from "./context/UserContext";
-import Sources from "./components/pages/sources.jsx"
-import Contact from "./components/pages/contact.jsx"
-
+import Sources from "./components/pages/sources.jsx";
+import Contact from "./components/pages/contact.jsx";
 
 function App() {
-  const path= window.location.pathname
-  
-   if (path == "/" || path == "select-history" ) {
+  const AnimatedGlobeRoutes = ["/", "/select-history", "/history"];
+  const { pathname } = useLocation();
+  console.log(useLocation());
   return (
-    <BrowserRouter>
-      <UserContextProvider>
-        <Navbar />
-        <main className="flex flex-col-reverse md:flex-row">
-          <Routes>
-            <Route path="/" element={<SignIn />} />
-            <Route path="history" element={<History />} />
-            <Route path="select-history" element={<UserSelectForm />} />
-            <Route path="about" element={<About/>} />
-            <Route path="sources" element={<Sources/>} />
-            <Route path="contact" element={<Contact/>} />
+    <UserContextProvider>
+      <Navbar />
+      <main className="flex flex-col-reverse md:flex-row">
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="history" element={<History />} />
+          <Route path="select-history" element={<UserSelectForm />} />
+          <Route path="about" element={<About />} />
+          <Route path="sources" element={<Sources />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+        {AnimatedGlobeRoutes.includes(pathname) && <AnimatedGlobe />}
+      </main>
 
-          </Routes>
-          <AnimatedGlobe />
-          
-        </main>
-        
-        <Footer />
-      </UserContextProvider>
-    </BrowserRouter>
+      <Footer />
+    </UserContextProvider>
   );
-   } else {
-    return( 
-      <BrowserRouter>
-      <UserContextProvider>
-        <Navbar />
-        <main className="flex flex-col-reverse md:flex-row">
-          <Routes>
-            <Route path="/" element={<SignIn />} />
-            <Route path="history" element={<History />} />
-            <Route path="select-history" element={<UserSelectForm />} />
-            <Route path="about" element={<About/>} />
-          </Routes>
-          
-          
-        </main>
-        
-        <Footer />
-      </UserContextProvider>
-    </BrowserRouter>
-    )
-   }
 }
 
 export default App;
