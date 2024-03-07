c40054f05a482bcbb4740e78059a37e2385fcc25
Toni97 <ayovcheva75@gmail.com>
2024-03-07 10:40:59 +0000

    adding souurces page

---
+++ src/components/pages/sources.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import myImage from "/teamwork.jpg"
+
+function Sources() {
+    return (
+      <div className="about-container">
+        <header>
+          <img src={mySources} alt="My Sources" className="sources-image" />
+          <h1>About us page </h1>
+        </header>
+        
+      </div>
+    );
+  }
+  
+  export default Sources;
