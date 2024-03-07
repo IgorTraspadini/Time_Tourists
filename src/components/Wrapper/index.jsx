de252b7db3159639461507fe7646db9bd99e3b6b
Rachel Atkinson <r.d.atkinson97@gmail.com>
2024-03-07 14:29:55 +0000

    fixed animated globe padding

--- src/components/Wrapper/index.jsx
+++ src/components/Wrapper/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 const Wrapper = React.forwardRef(({ children, className }, ref) => {
   // Combines existing classes with any classes passed via className prop
-  return <div ref={ref} className={`w-full md:w-1/2 md:min-h-[85vh] p-4 ${className}`}>{children}</div>;
+  return <div ref={ref} className={`w-full md:w-1/2 md:min-h-[85vh] p-0 ${className}`}>{children}</div>;
 });
 export default Wrapper;
