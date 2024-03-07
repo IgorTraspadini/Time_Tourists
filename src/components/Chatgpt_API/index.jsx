5c9a1acf5fad15ec1d80f1aae352566397e9c5df
IgorTraspadini <igorchieppe@hotmail.com>
2024-03-07 16:47:01 -0300

    change the env token key name - Test

--- src/components/Chatgpt_API/index.jsx
+++ src/components/Chatgpt_API/index.jsx
@@ -15,7 +15,7 @@ function ChatComponent(props) {
     }
 
     const openai = new OpenAI({
-      apiKey: process.env.REACT_APP_GREETING,
+      apiKey: import.meta.env.REACT_APP_GREETING,
       dangerouslyAllowBrowser: true,
     });
 
