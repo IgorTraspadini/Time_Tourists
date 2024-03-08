e1aa8df8df93b085152fc6915e0f4c9cf41d64c7
IgorTraspadini <igorchieppe@hotmail.com>
2024-03-08 12:04:08 -0300

    Test-Netlify-function-8

--- src/assets/js/get_token.js
+++ src/assets/js/get_token.js
@@ -3,7 +3,6 @@ export async function getToken() {
   try {
       const response = await fetch(url);
       const data = await response.json();
-      //const data = await response.json();
       return data.token;
   } catch (err) {
       console.log(err);
